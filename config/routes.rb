835aca7bc25cd1d52f86a52852aa383e10be8f17
Johan Sim Jian An <johansja@gmail.com>
2011-10-28 15:17:48 +0800

    Imported clearance views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 StockIO::Application.routes.draw do
-  resources :products
-
   get "home/index"
-
-  resources :product_categories
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -17,7 +13,8 @@ StockIO::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :products
+  resources :product_categories
 
   # Sample resource route with options:
   #   resources :products do
